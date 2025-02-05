2a2cf250df60a9e8c218e2a8234226b74aac7593
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-02-05 16:31:48 +0100

    Get all products route handler

---
+++ app/api/get-products/route.ts
@@ -0,0 +1,15 @@
+import { connectDB } from "../db/connectDB";
+import Product from "../models/product.model";
+
+export async function GET(request: Request) {
+  await connectDB();
+
+  try {
+    const products = await Product.find({}).sort({ createdAt: -1 });
+
+    return Response.json({ products }, { status: 200 });
+  } catch (error: any) {
+    console.log("Error in getting products", error);
+    return Response.json({ message: error.message }, { status: 400 });
+  }
+}
