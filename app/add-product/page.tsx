85f701b4b6a21bee9be7599f6e9a359846406e62
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-02-05 16:07:34 +0100

    Used server actions to store add products

--- app/add-product/page.tsx
+++ app/add-product/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import React from "react";
+import Form from "../components/Form";
 
 const AddProduct = () => {
   return (
@@ -8,62 +8,7 @@ const AddProduct = () => {
         Add a new product
       </h2>
 
-      <form className="w-full max-w-xl mx-auto flex flex-col justify-center items-center space-y-4 mt-3 md:mt-5">
-        <Image
-          src="/watch-img.jpg"
-          alt="img"
-          width={1000}
-          height={1000}
-          className="max-w-full max-h-72 object-cover object-center rounded-lg"
-        />
-        <div className="flex flex-col w-full">
-          <label className="">Product Image:</label>
-          <input
-            type="file"
-            accept="image/*"
-            className="w-full px-3 py-1.5 md:py-2 text-[#252422] rounded-lg  bg-white border border-gray-500"
-          />
-        </div>
-
-        <div className="flex flex-col w-full">
-          <label className="">Name:</label>
-          <input
-            type="text"
-            placeholder="Enter the product name"
-            className="w-full px-3 py-1.5 md:py-2 text-[#252422] rounded-lg bg-white border border-gray-500"
-          />
-        </div>
-        <div className="flex flex-col w-full">
-          <label className="">Price:</label>
-          <input
-            type="number"
-            placeholder="Enter the product price"
-            className="w-full px-3 py-1.5 md:py-2 text-[#252422] rounded-lg bg-white border border-gray-500"
-          />
-        </div>
-        <div className="flex flex-col w-full">
-          <label className="">Seller's Link:</label>
-          <input
-            type="text"
-            placeholder="Link to where buyers can find you"
-            className="w-full px-3 py-1.5 md:py-2 text-[#252422] rounded-lg bg-white border border-gray-500"
-          />
-        </div>
-        <div className="flex flex-col w-full">
-          <label className="">Description:</label>
-          <textarea
-            rows={4}
-            placeholder="Enter the product description"
-            className="w-full px-3 py-1.5 resize-none md:py-2 text-[#252422] rounded-lg bg-white border border-gray-500"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-[#212529] hover:bg-[#343A40] text-white px-3 py-2 rounded-md"
-        >
-          Add Product
-        </button>
-      </form>
+      <Form />
     </div>
   );
 };
