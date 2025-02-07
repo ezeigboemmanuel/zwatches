158b1cafc81444fd0314f1f17c7f1e077ceb2b14
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-02-07 12:29:43 +0100

    wrapped in suspense

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import { useParams, useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 
 interface Product {
   image: string;
@@ -37,33 +37,35 @@ const SearchPage = () => {
     return <p>Loading...</p>;
   }
   return (
-    <div
-      className="px-4 md:px-12 py-5 md:py-10 flex justify-center items-center"
-      id="product"
-    >
-      {products.length > 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-          {products.map((product: Product, index) => (
-            <Link href={`/product/${product._id}`} key={index}>
-              <Image
-                src={product.image}
-                alt="img"
-                width={1000}
-                height={1000}
-                className="max-w-[17rem] min-h-72 max-h-72 object-cover object-center rounded-lg"
-              />
+    <Suspense>
+      <div
+        className="px-4 md:px-12 py-5 md:py-10 flex justify-center items-center"
+        id="product"
+      >
+        {products.length > 0 ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+            {products.map((product: Product, index) => (
+              <Link href={`/product/${product._id}`} key={index}>
+                <Image
+                  src={product.image}
+                  alt="img"
+                  width={1000}
+                  height={1000}
+                  className="max-w-[17rem] min-h-72 max-h-72 object-cover object-center rounded-lg"
+                />
 
-              <div className="mt-4">
-                <h2 className="font-semibold text-lg">{product.name}</h2>
-                <p className="font-medium text-sm mt-1">${product.price}</p>
-              </div>
-            </Link>
-          ))}
-        </div>
-      ) : (
-        <p>No products found</p>
-      )}
-    </div>
+                <div className="mt-4">
+                  <h2 className="font-semibold text-lg">{product.name}</h2>
+                  <p className="font-medium text-sm mt-1">${product.price}</p>
+                </div>
+              </Link>
+            ))}
+          </div>
+        ) : (
+          <p>No products found</p>
+        )}
+      </div>
+    </Suspense>
   );
 };
 
