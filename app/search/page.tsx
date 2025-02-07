ee3e04b73737aba116ca3358f6e126fdce6d4fd8
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2025-02-07 12:22:46 +0100

    Search bar implemented

---
+++ app/search/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
+import { useParams, useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+interface Product {
+  image: string;
+  _id: string;
+  name: string;
+  price: number;
+}
+
+const SearchPage = () => {
+  const [products, setProducts] = useState([]);
+
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    console.log(searchTermFromUrl);
+
+    if (searchTermFromUrl) {
+      const searchQuery = urlParams.toString();
+      //   searchBooks(searchQuery);
+
+      axios
+        .get(`/api/search?${searchQuery}`)
+        .then((response) => setProducts(response.data.products));
+    }
+  }, [searchParams]);
+
+  if (!products) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <div
+      className="px-4 md:px-12 py-5 md:py-10 flex justify-center items-center"
+      id="product"
+    >
+      {products.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+          {products.map((product: Product, index) => (
+            <Link href={`/product/${product._id}`} key={index}>
+              <Image
+                src={product.image}
+                alt="img"
+                width={1000}
+                height={1000}
+                className="max-w-[17rem] min-h-72 max-h-72 object-cover object-center rounded-lg"
+              />
+
+              <div className="mt-4">
+                <h2 className="font-semibold text-lg">{product.name}</h2>
+                <p className="font-medium text-sm mt-1">${product.price}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <p>No products found</p>
+      )}
+    </div>
+  );
+};
+
+export default SearchPage;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,18 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { ChangeEvent, useState } from "react";
 
 const Navbar = () => {
+  const router = useRouter();
+  const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", e.target.value);
+
+    const searchQuery = urlParams.toString();
+    router.push(`/search?${searchQuery}`);
+  };
   return (
     <nav className="px-4 md:px-12 py-4 md:py-6 bg-white text-black">
       <div className="flex justify-between items-center">
@@ -31,6 +42,7 @@ const Navbar = () => {
             className="h-[36px] relative pl-10 border-[1px] border-black/[0.7] text-sm rounded-[8px] w-full py-2 px-3 focus:outline-none focus:shadow-outline bg-transparent"
             id="links"
             type="text"
+            onChange={handleChange}
             placeholder="Search"
           />
         </div>
